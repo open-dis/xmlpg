1b9703c05ff00d8a0d4da68038d7d4892c2ab8b7
Leif Gruenwoldt <leifer@gmail.com>
2019-06-01 09:45:20 -0400

    update runXmlpgsh, jaxb and hibernate are off by default now

--- runXmlpg.sh
+++ runXmlpg.sh
@@ -8,6 +8,6 @@ mvn clean compile
 
 for lang in java cpp python javascript objc csharp; do
 
-mvn exec:java -Dexec.mainClass=edu.nps.moves.xmlpg.Xmlpg  -Dxmlpg.useHibernate=false -Dxmlpg.useJaxb=false -Dxmlpg.generatedSourceDir=target/generated-sources/xmlpg-`basename $schema .xml`-$lang/ -Dexec.args="$schema $lang"
+mvn exec:java -Dexec.mainClass=edu.nps.moves.xmlpg.Xmlpg  -Dxmlpg.generatedSourceDir=target/generated-sources/xmlpg-`basename $schema .xml`-$lang/ -Dexec.args="$schema $lang"
 
 done;
